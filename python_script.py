d53b31cae83118e0b69a04578d6d7e6156e92882
Felipe <rdrgues@hotmail.com>
2022-01-12 17:35:48 -0700

    new version of python script

--- python_script.py
+++ python_script.py
@@ -1,5 +1,7 @@
 import requests
 
-print(requests.__version__)
+# print(requests.__version__)
 
-r = requests.get("http://google.com/")
+r = requests.get("https://raw.githubusercontent.com/3662/cmput_404_lab_1/master/python_script.py")
+
+print(r.text)
